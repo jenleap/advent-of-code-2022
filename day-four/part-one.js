c6455b4afaeb3ca79b9a9d2e2a8e13333c308dec
jenleap <jenleap@gmail.com>
2022-12-04 14:51:41 -0500

    day four solutioon

---
+++ day-four/part-one.js
@@ -0,0 +1,20 @@
+const { strFileHelper } = require('../file-helper');
+
+strFileHelper('input.txt').then(fileOutput => {
+    let counter = 0;
+   
+    fileOutput.forEach(pair => {
+        let elfPairs = pair.split(',');
+        elfPairs = elfPairs.map(elf => elf.split('-').map(num => parseInt(num)));
+
+        if (elfPairs[1]) {
+            if ((elfPairs[0][0] <= elfPairs[1][0] && elfPairs[0][1] >= elfPairs[1][1]) ||
+                (elfPairs[0][0] >= elfPairs[1][0] && elfPairs[0][1] <= elfPairs[1][1])
+            ) {
+                counter++;
+            } 
+        }
+    });
+
+    console.log(counter);
+});
